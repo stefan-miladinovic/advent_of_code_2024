2415ae082c9a8fe2ba8851109beb7e7cdb299741
smiladinovic <smiladinovic87@gmail.com>
2024-12-30 11:43:41 +0100

    Solution for Part one day 4

---
+++ 04-four/four_solution.py
@@ -0,0 +1,45 @@
+"""
+However, because the program's memory has been corrupted, there are also many invalid characters that should be ignored,
+even if they look like part of a mul instruction. Sequences like mul(4*, mul(6,9!, ?(12,34), or mul ( 2 , 4 ) do nothing.
+
+For example, consider the following section of corrupted memory:
+
+xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+Only the four highlighted sections are real mul instructions. Adding up the result of each instruction produces 161 (2*4 + 5*5 + 11*8 + 8*5).
+
+Scan the corrupted memory for uncorrupted mul instructions. What do you get if you add up all of the results of the multiplications?
+"""
+
+import re
+
+# get the data from file
+filename = "input_four"
+reg_x = r"(mul\(\d+\,\d+\))"  # needs to be declared as raw string to prevent illegal escape sequence for \d etc.
+
+with open(filename) as f:
+    input_data = f.read()
+
+correct_muls = re.findall(reg_x, input_data)  # returns the list of all correct `mul` operators
+
+
+def calculate_final_result(muls_list) -> int:
+    nums_from_expr = []
+    for row in muls_list:
+        if row is None:
+            continue
+        else:
+            _strip_1 = row.strip('mul(')
+            _strip_2 = _strip_1.strip(')')
+            for i, char in enumerate(_strip_2):
+                if char == ',':
+                    _num_1 = int(_strip_2[:i])
+                    _num_2 = int(_strip_2[i + 1:])
+                    _calc = _num_1 * _num_2
+                    nums_from_expr.append(_calc)
+    _final_result = 0
+    for row in nums_from_expr:
+        _final_result += row
+    return _final_result
+
+
+print(f"The final result is {calculate_final_result(correct_muls)}")
